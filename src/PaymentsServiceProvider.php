84b5e2f4bb2e154b300efa483fbb7ffddabaa8e3
Drew Roberts <github@drewroberts.com>
2021-02-13 10:30:00 -0500

    Remove policies since not created yet

--- src/PaymentsServiceProvider.php
+++ src/PaymentsServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Payments;
 
-use Tipoff\Payments\Models\Payment;
-use Tipoff\Payments\Policies\PaymentPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -14,9 +12,6 @@ class PaymentsServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                Payment::class => PaymentPolicy::class,
-            ])
             ->name('payments')
             ->hasConfigFile();
     }
