8424eeda8baa137e40ae394132c675a662d585a7
Patrick Breen <patrick.breen@quartzy.com>
2021-03-11 16:03:00 -0500

    Register payment gateway and add tests

--- src/Services/PaymentGateway/StripePaymentGateway.php
+++ src/Services/PaymentGateway/StripePaymentGateway.php
@@ -15,18 +15,23 @@ class StripePaymentGateway implements PaymentGateway
 {
     public function charge(Location $location, User $user, int $amount, array $options = []): object
     {
-        if ($paymentSettings = LocationPaymentSetting::forLocation($location)) {
-            try {
-                Stripe::setApiKey($paymentSettings->stripe_secret);
-
-                return $user->charge($amount, $options['payment_method_id'], [
-                    'description' => $options['description'] ?? '',
-                ]);
-            } catch (Exception $exception) {
-                throw new PaymentChargeException($exception->getMessage());
-            }
+        $paymentSettings = LocationPaymentSetting::forLocation($location);
+        if (!$paymentSettings || !$paymentSettings->stripe_secret) {
+            throw new PaymentChargeException('Stripe not configured for location.');
         }
 
-        throw new PaymentChargeException('Stripe not configured for location');
+        if (empty($options['payment_method_id'])) {
+            throw new PaymentChargeException('Payment method is required.');
+        }
+
+        try {
+            Stripe::setApiKey($paymentSettings->stripe_secret);
+
+            return $user->charge($amount, $options['payment_method_id'], [
+                'description' => $options['description'] ?? '',
+            ]);
+        } catch (Exception $exception) {
+            throw new PaymentChargeException($exception->getMessage());
+        }
     }
 }
---
+++ tests/Unit/Services/StripePaymentGatewayTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Payments\Tests\Unit\Services;
+
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Authorization\Models\User;
+use Tipoff\Locations\Models\Location;
+use Tipoff\Payments\Exceptions\PaymentChargeException;
+use Tipoff\Payments\Models\LocationPaymentSetting;
+use Tipoff\Payments\Services\PaymentGateway\PaymentGateway;
+use Tipoff\Payments\Tests\TestCase;
+
+class StripePaymentGatewayTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function charge_no_location_config()
+    {
+        $user = User::factory()->create();
+        $location = Location::factory()->create();
+
+        $service = $this->app->make(PaymentGateway::class);
+
+        $this->expectException(PaymentChargeException::class);
+        $this->expectExceptionMessage('Stripe not configured for location.');
+
+        $service->charge($location, $user, 123, ['payment_method_id' => 'abcd']);
+    }
+
+    /** @test */
+    public function charge_no_stripe_config()
+    {
+        $user = User::factory()->create();
+        $location = Location::factory()->create();
+        LocationPaymentSetting::factory()->create([
+            'location_id' => $location,
+            'stripe_publishable' => null,
+            'stripe_secret' => null,
+        ]);
+
+        $service = $this->app->make(PaymentGateway::class);
+
+        $this->expectException(PaymentChargeException::class);
+        $this->expectExceptionMessage('Stripe not configured for location.');
+
+        $service->charge($location, $user, 123, ['payment_method_id' => 'abcd']);
+    }
+
+    /** @test */
+    public function charge_no_paymethod_id()
+    {
+        $user = User::factory()->create();
+        $location = Location::factory()->create();
+        LocationPaymentSetting::factory()->create([
+            'location_id' => $location,
+            'stripe_publishable' => 'PUB',
+            'stripe_secret' => 'SEC',
+        ]);
+
+        $service = $this->app->make(PaymentGateway::class);
+
+        $this->expectException(PaymentChargeException::class);
+        $this->expectExceptionMessage('ayment method is required.');
+
+        $service->charge($location, $user, 123, []);
+    }
+
+    /** @test */
+    public function charge_ok()
+    {
+        $location = Location::factory()->create();
+        LocationPaymentSetting::factory()->create([
+            'location_id' => $location,
+            'stripe_publishable' => 'PUB',
+            'stripe_secret' => 'SEC',
+        ]);
+
+        $service = $this->app->make(PaymentGateway::class);
+
+        $user = \Mockery::mock(User::class);
+        $user->shouldReceive('charge')
+            ->withArgs(function ($amount, $paymethod, $other) {
+                return $amount === 123 && $paymethod === 'abcd';
+            })
+            ->once()
+            ->andReturn((object) []);
+
+        $service->charge($location, $user, 123, ['payment_method_id' => 'abcd']);
+    }
+}
